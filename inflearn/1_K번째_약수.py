696289024d5854ae2e82071c19ec58cbff9ed63f
codenavy94 <codenavy94@yonsei.ac.kr>
2022-04-04 23:30:41 +0900

    q1 update

--- inflearn/1_K번째_약수.py
+++ inflearn/1_K번째_약수.py
@@ -8,9 +8,13 @@ for i in range(1, sqrt_N+1):
         divisor_lst.append(i)
         divisor_lst.append(q)
     
-try:
+# 중복되는 약수 제거
+divisor_lst = list(set(divisor_lst))
+
+# K가 약수의 개수 이상일 때에만 result 존재
+if len(divisor_lst) >= K:
     result = sorted(divisor_lst)[K-1]
-except:
+else:
     result = -1
     
 print(result)
