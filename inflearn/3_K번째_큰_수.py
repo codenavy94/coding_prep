93510f118c2ca73202ac3d56e3bbf89ef68b01e4
codenavy94 <codenavy94@yonsei.ac.kr>
2022-04-05 00:13:05 +0900

    q3 solution update

--- inflearn/3_K번째_큰_수.py
+++ inflearn/3_K번째_큰_수.py
@@ -1,7 +1,7 @@
 from itertools import combinations
 
 N, K = map(int, input().split())
-cards = sorted(list(map(int, input().split())), reverse=True)
+cards = list(map(int, input().split()))
 sum_results = []
 
 for comb in combinations(cards, 3):
@@ -9,3 +9,19 @@ for comb in combinations(cards, 3):
         sum_results.append(sum(comb))
 
 print(sorted(sum_results, reverse=True)[K-1])
+
+
+
+### Another Solution ###
+
+# n, k = map(int, input().split())
+# a = list(map(int, input().split()))
+# res = set()
+
+# for i in range(n-2):
+#     for j in range(i+1, n-1):
+#         for m in range(j+1, n):
+#             res.add(a[i]+a[j]+a[m])
+            
+# res = sorted(list(res), reverse=True)
+# print(res[k-1])
