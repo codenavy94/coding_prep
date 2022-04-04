8a87cb5f4801a03d74ebe2b0b911341a15d7204d
codenavy94 <codenavy94@yonsei.ac.kr>
2022-04-04 23:57:37 +0900

    q1 update

--- inflearn/1_K번째_약수.py
+++ inflearn/1_K번째_약수.py
@@ -18,3 +18,22 @@ else:
     result = -1
     
 print(result)
+
+
+### Another solution ###
+
+# import sys
+# sys.stdin=open('input.txt', 'rt')
+
+# n, k = map(int, input().split())
+# cnt = 0
+
+# for i in range(1, n+1):
+#     if n % i == 0:
+#         cnt += 1
+#     if cnt == k:
+#         print(i)
+#         break
+    
+# else:
+#     print(-1)
