de169082e7d918daeb0c5b8e8127bab916af2cd6
codenavy94 <codenavy94@yonsei.ac.kr>
2022-04-06 00:00:27 +0900

    solution updates

--- inflearn/4_대표값.py
+++ inflearn/4_대표값.py
@@ -1,3 +1,15 @@
+"""
+Python에서 round는 round_half_even 방식을 택한다.
+예: a = 66.5 일 때 round(a) 하면 66이 됨
+
+Alternative:
+0.5 더한 뒤 int()로 소수점 아래 버림하기
+
+a = 66.5
+a += 0.5
+a = int(a)
+"""
+
 N = int(input())
 scores = list(map(int, input().split()))
 mean_score = int(round(sum(scores)/N, 0))
@@ -18,3 +30,27 @@ for i in range(N):
             index = i+1
             
 print(mean_score, index)
+
+
+
+### Another Solution ###
+
+# n = int(input())
+# a = list(map(int, input().split()))
+# ave = int(sum(a)/n + 0.5)
+# min = 2147000000
+
+# for idx, x in enumerate(a):
+#     tmp = abs(x - ave)
+    
+#     if tmp < min:
+#         min = tmp
+#         score = x
+#         res = idx + 1
+        
+#     elif tmp == min:
+#         if x > score:
+#             score = x
+#             res = idx + 1
+            
+# print(ave, res)
--- inflearn/5_정다면체.py
+++ inflearn/5_정다면체.py
@@ -14,3 +14,24 @@ for k, v in combinations:
         print(k, end=' ')
     else:
         break
+
+
+
+### 2nd Solution ###
+
+N, M = map(int, input().split())
+cnt_lst = [0] * (N+M+1)
+_max = -2147000000
+
+for n in range(1, N+1):
+    for m in range(1, M+1):
+        _sum = n+m
+        cnt_lst[_sum] += 1
+        
+for i in range(N+M+1):
+    if cnt_lst[i] > max:
+        _max = cnt_lst[i]
+
+for idx, cnt in enumerate(cnt_lst):
+    if cnt == _max:
+        print(idx, end=' ')
