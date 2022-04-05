01ac4d67a814355c85b470ffd8087025ef5b5a2a
codenavy94 <codenavy94@yonsei.ac.kr>
2022-04-06 00:09:13 +0900

    solution update

--- inflearn/6_자릿수의_합.py
+++ inflearn/6_자릿수의_합.py
@@ -1,12 +1,39 @@
+def digit_sum(x:str):
+    return sum(map(int, list(x)))
+
 N = int(input())
 num_lst = input().split()
 max = 0
 ans = 0
 
 for num in num_lst:
-    num_sum = sum(map(int, list(num)))
+    num_sum = digit_sum(num)
     if num_sum > max:
         max = num_sum
         ans = int(num)
         
 print(ans)
+
+
+
+### Another Solution ###
+
+n = int(input())
+a = list(map(int, input().split()))
+
+def digit_sum(x):
+    sum = 0
+    while x > 0:
+        sum += x % 10
+        x = x // 10
+    return sum
+
+max = -2147000000
+
+for x in a:
+    tot = digit_sum(x)
+    if tot > max:
+        max = tot
+        res = x
+
+print(res)
