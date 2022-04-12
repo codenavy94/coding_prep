6a3aa77d6831f833a085509ec1244ca4bd881955
codenavy94 <codenavy94@yonsei.ac.kr>
2022-04-12 17:57:18 +0900

    solution update

--- inflearn/13_카드_역배치.py
+++ inflearn/13_카드_역배치.py
@@ -2,7 +2,8 @@ cards = list(range(21))
 
 for _ in range(10):
     start, end = map(int, input().split())
-    cards[start:end+1] = cards[start:end+1][::-1]
+    for i in range((end-start+1)//2):
+        cards[start+i], cards[end-i] = cards[end-i], cards[start+i]
 
 cards = list(map(str, cards))
 print(' '.join(cards[1:]))
--- inflearn/14_두_리스트_합치기.py
+++ inflearn/14_두_리스트_합치기.py
@@ -16,3 +16,34 @@ res_lst.extend(N_lst)
 res_lst = list(map(str, res_lst))
 
 print(' '.join(res_lst))
+
+
+
+### Another Solution ###
+# 포인터 이용하기
+
+M = int(input())
+M_lst = list(map(int, input().split()))
+N = int(input())
+N_lst = list(map(int, input().split()))
+
+M_pointer, N_pointer = 0, 0
+res_lst = []
+
+while (M_pointer < M) and (N_pointer < N):
+    if M_lst[M_pointer] <= N_lst[N_pointer]:
+        res_lst.append(M_lst[M_pointer])
+        M_pointer += 1
+        
+    else:
+        res_lst.append(N_lst[N_pointer])
+        N_pointer += 1
+        
+if M_pointer == M:
+    res_lst.extend(N_lst[N_pointer:])
+elif N_pointer == N:
+    res_lst.extend(M_lst[M_pointer:])
+
+res_lst = list(map(str, res_lst))
+
+print(' '.join(res_lst))
