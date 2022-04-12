ca688a215a6dd1195d1d5c213192c30c227e1a2f
codenavy94 <codenavy94@yonsei.ac.kr>
2022-04-12 14:51:50 +0900

    q12 update

--- inflearn/12_숫자만_추출.py
+++ inflearn/12_숫자만_추출.py
@@ -15,3 +15,20 @@ for i in range(1, int(res**0.5)+1):
         
 print(res)
 print(len(divisor_set))
+
+
+
+### Another Solution ###
+# s = input()
+# res = 0
+# for x in s:
+#     if x.isdecimal():
+#         res = res * 10 + int(x)
+        
+# cnt = 0
+# for i in range(1, res+1):
+#     if res % i == 0:
+#         cnt += 1
+        
+# print(res)
+# print(cnt)
