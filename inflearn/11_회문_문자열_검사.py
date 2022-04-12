377a0d9d1c6e76943e7454a079cac47edfa964d8
codenavy94 <codenavy94@yonsei.ac.kr>
2022-04-12 14:43:20 +0900

    q11 update

--- inflearn/11_회문_문자열_검사.py
+++ inflearn/11_회문_문자열_검사.py
@@ -6,3 +6,19 @@ for t in range(1, T+1):
     if input_str == input_str[::-1]:
         flag = 'YES'
     print(f"#{t} {flag}")
+    
+    
+
+### Another Solution ###
+# n = int(input())
+# for i in range(n):
+#     s = input()
+#     s = s.upper()
+#     size = len(s)
+    
+#     for j in range(size//2):
+#         if s[j] != s[-1-j]:
+#             print(f"#{n+1} NO")
+#             break
+#     else:
+#         print(f"#{n+1} YES")
