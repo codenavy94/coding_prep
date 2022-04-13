e873cbadd57b716c945f90ad5f67c15138ec40b5
codenavy94 <codenavy94@yonsei.ac.kr>
2022-04-13 15:28:21 +0900

    q17 update

--- inflearn/17_사과나무(다이아몬드).py
+++ inflearn/17_사과나무(다이아몬드).py
@@ -8,3 +8,23 @@ for i in range(1, N+1):
     tot += sum(mat[i-1][diff:N-diff])
     
 print(tot)
+
+
+
+### Another Solution ###
+
+n = int(input())
+a = [list(map(int, input().split())) for _ in range(n)]
+res = 0
+s=e=n//2
+
+for i in range(n):
+    for j in range(s, e+1):
+        res += a[i][j]
+    if i < n//2:
+        s -= 1
+        e += 1
+    else:
+        s += 1
+        e -= 1
+print(res)
