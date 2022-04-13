5289c6eb2f9ce6b38ef20c0ac1909d19b2637863
codenavy94 <codenavy94@yonsei.ac.kr>
2022-04-13 15:41:47 +0900

    q18 update

--- inflearn/18_곳감(모래시계).py
+++ inflearn/18_곳감(모래시계).py
@@ -32,3 +32,35 @@ for i in range(N):
     tot += sum(mat[i][(mid-diff) : N-(mid-diff)])
     
 print(tot)
+
+
+
+### Another Solution ###
+
+n = int(input())
+a = [list(map(int, input().split())) for _ in range(n)]
+m = int(input())
+
+for i in range(m):
+    h, t, k = map(int, input().split())
+    if t == 0: # rotate to left
+        for _ in range(k):
+            a[h-1].append(a[h-1].pop(0))
+    else:
+        for _ in range(k):
+            a[h-1].insert(0, a[h-1].pop())
+            
+s = 0
+e = n-1
+res = 0
+for i in range(n):
+    for j in range(s, e+1):
+        res += a[i][j]
+    if i < n//2:
+        s += 1
+        e -= 1
+    else:
+        s -= 1
+        e += 1
+        
+print(res)
