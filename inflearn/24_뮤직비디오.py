4c97ae09cce63ec7d17d7b6d1040e67cf6a12ba5
codenavy94 <codenavy94@yonsei.ac.kr>
2022-04-21 15:14:46 +0900

    q24 update

--- inflearn/24_뮤직비디오.py
+++ inflearn/24_뮤직비디오.py
@@ -3,8 +3,7 @@ sys.stdin = open("input.txt", "r")
 
 n, m = map(int, input().split())
 album = list(map(int, input().split()))
-q, r = divmod(n, m)
-max_item = q+r
+res = 0
 
 def Check(min_len):
     tmp = album.copy()
@@ -20,15 +19,48 @@ def Check(min_len):
             sum = tmp_item
     return cnt + 1 
         
-lt = sum(sorted(album)[:q])
-rt = sum(sorted(album)[-1*max_item:])
+lt = 1
+rt = sum(album)
+maxx = max(album)
 
 while lt <= rt:
     mid = (lt+rt)//2
-    res = Check(mid)
-    if res > m:
-        lt = mid+1
-    else:
+    if mid >= maxx and Check(mid) <= m:
+        res = mid
         rt = mid-1
+    else:
+        lt = mid+1
 
-print(mid)
+print(res)
+
+
+
+### Another Solution ###
+
+# def Count(capacity):
+#     cnt = 1
+#     sum = 0
+#     for x in Music:
+#         if sum + x > capacity:
+#             cnt += 1
+#             sum = x
+#         else:
+#             sum += x
+#     return cnt
+
+# n, m = map(int, input().split())
+# Music = list(map(int, input().split()))
+# maxx = max(Music)
+# lt = 1
+# rt = sum(Music)
+# res = 0
+
+# while lt <= rt:
+#     mid = (lt+rt)//2
+#     if mid >= maxx and Count(mid) <= m:
+#         res = mid
+#         rt = mid-1
+#     else:
+#         lt = mid+1
+
+# print(res)
