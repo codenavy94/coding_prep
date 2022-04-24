e5e37fe2b75a2f4d5eb16c74f76744d8a5d2df78
codenavy94 <codenavy94@yonsei.ac.kr>
2022-04-24 19:27:23 +0900

    q25 update

--- inflearn/25_마구간_정하기.py
+++ inflearn/25_마구간_정하기.py
@@ -1,28 +1,31 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
+def Count(len):
+    cnt = 1
+    ep = loc[0]
+    for i in range(1, n-1):
+        if loc[i] - ep >= len:
+            cnt += 1
+    return cnt
+
 n, c = map(int, input().split())
 loc = [int(input()) for _ in range(n)]
 loc.sort()
 
-lt = min_loc = loc.pop(0) # minimum distance
+min_loc = loc.pop(0)
+lt = 1 # minimum distance
 rt = loc[-1] # maximum distance
 
 while lt <= rt:
     cnt = 1
     mid = (lt + rt) // 2
-    tmp = min_loc
     
-    for l in loc: # 2 4 8 9
-        if l - tmp >= mid: # 8
-            cnt += 1
-            tmp = l
-            
-    if cnt >= c:
+    if Count(mid) >= c:
         res = mid
         lt = mid + 1
         
     else:
-        rt = mid - 1
+        rt = mid -1
              
 print(res)
