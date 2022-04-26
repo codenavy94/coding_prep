2fead1ddef82d507b3a45fc1ffe85d3e9117ed54
codenavy94 <codenavy94@yonsei.ac.kr>
2022-04-26 16:57:57 +0900

    q29 update

--- inflearn/29_침몰하는_타이타닉.py
+++ inflearn/29_침몰하는_타이타닉.py
@@ -1,12 +1,43 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
 n, m = map(int, input().split())
-passengers = list(map(int, input().split()))
-passengers.sort()
+p = list(map(int, input().split()))
+p.sort()
 
 cnt = 0
-tmp = 0
-for p in passengers:
-    tmp += p
-    
-    if tmp > m:
+while len(p) >= 2:
+    p_sum = p[0] + p[-1]
+    if p_sum > m:
         cnt += 1
-        tmp = m
+        p.pop()
+    else:
+        cnt += 1
+        p.pop(0)
+        p.pop()
+cnt += len(p)
+
+print(cnt)
+
+
+
+### Another Solution ###
+
+n, limit = map(int, input().split())
+p = list(map(int, input().split()))
+p.sort()
+cnt = 0
+
+while p:
+    if len(p) == 1:
+        cnt += 1
+        break
+    if p[0]+p[-1] > limit:
+        p.pop()
+        cnt += 1
+    else:
+        p.pop(0)
+        p.pop()
+        cnt += 1
+        
+print(cnt)
