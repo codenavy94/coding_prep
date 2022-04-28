11e166992aefd8d243d12a0543e117e4fc02c5d8
codenavy94 <codenavy94@yonsei.ac.kr>
2022-04-28 15:57:16 +0900

    q36 update

--- inflearn/36_공주구하기.py
+++ inflearn/36_공주구하기.py
@@ -1,9 +1,20 @@
-import sys
 from collections import deque
-sys.stdin = open("input.txt", "r")
 
 n, k = map(int, input().split())
 a = deque()
 for i in range(1, n+1):
     a.append(i)
+
+cnt = 0
+while True:
+    a.append(a.popleft())
+    cnt += 1
+    
+    if cnt == k and len(a) > 1:
+        a.pop()
+        cnt = 0
+    elif len(a) == 1:
+        break
+    
+print(a[0])
     
